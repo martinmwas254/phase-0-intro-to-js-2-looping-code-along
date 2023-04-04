b640f1cd2a2793857e634134a9dd2882897a89af
martinmwas254 <martinmwas264@gmail.com>
2023-04-04 15:08:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+function writeCards(names, event) {
+  const birthdayMessage = [];
+
+  for (let i = 0; i < names.length; i++) {
+    birthdayMessage.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return birthdayMessage;
+}
+
+function Cards(names) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}!`);
+  }
+  return messages;
+}
